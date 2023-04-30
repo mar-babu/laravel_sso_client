17b5da4205098888f230a53224a2e214be4e727a
mar-babu <ar_cse@yahoo.com>
2023-04-30 20:14:38 +0600

    login oauth server > callback response to client app

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use \Illuminate\Support\Facades\Http;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +18,35 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/login', function (Request $request) {
+    $request->session()->put("state", $state = Str::random(40));
+    $query = http_build_query([
+        "client_id" => "990e2d78-fc55-4382-9032-5022ba6c1a40",
+        "redirect_url" => "http://127.0.0.1:8080/callback",
+        "response_type" => "code",
+        "scope" => "",
+        "state" => $state,
+    ]);
+    return redirect("http://127.0.0.1:8000/oauth/authorize?" . $query);
+});
+
+Route::get('/callback', function (Request $request) {
+    $state = $request->session()->pull("state");
+
+    throw_unless(strlen($state) > 0 && $state == $request->state,
+    InvalidArgumentException::class);
+
+    $response = Http::asForm()->post(
+        "http://127.0.0.1:8000/oauth/token/",
+        [
+        "grant_type" => "authorization_code",
+        "client_id" => "990e2d78-fc55-4382-9032-5022ba6c1a40",
+        "client_secret" => "kUYg8ze5Ajg7h6Z1AcC53V6aGrcArnjfZ1vWee6F",
+        "redirect_url" => "http://127.0.0.1:8080/callback",
+        "code" => $request->code
+
+    ]);
+    return $response->json();
 });
