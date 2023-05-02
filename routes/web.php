90dbb2d445ef9559adc306ff7a113edd4ff2b0aa
mar-babu <ar_cse@yahoo.com>
2023-05-02 12:30:31 +0600

    update

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/', function () {
 Route::get('/login', function (Request $request) {
     $request->session()->put("state", $state = Str::random(40));
     $query = http_build_query([
-        "client_id" => "990e2d78-fc55-4382-9032-5022ba6c1a40",
+        "client_id" => "99118fdc-6765-404c-8dc6-08c37146e121",
         "redirect_url" => "http://127.0.0.1:8080/callback",
         "response_type" => "code",
         "scope" => "",
@@ -35,15 +35,15 @@ Route::get('/login', function (Request $request) {
 Route::get('/callback', function (Request $request) {
     $state = $request->session()->pull("state");
 
-    throw_unless(strlen($state) > 0 && $state == $request->state,
-    InvalidArgumentException::class);
+    /*throw_unless(strlen($state) > 0 && $state === $request->state,
+    InvalidArgumentException::class);*/
 
     $response = Http::asForm()->post(
         "http://127.0.0.1:8000/oauth/token/",
         [
         "grant_type" => "authorization_code",
-        "client_id" => "990e2d78-fc55-4382-9032-5022ba6c1a40",
-        "client_secret" => "kUYg8ze5Ajg7h6Z1AcC53V6aGrcArnjfZ1vWee6F",
+        "client_id" => "99118fdc-6765-404c-8dc6-08c37146e121",
+        "client_secret" => "hHQ3hGYpgAQEwZvVIYieG8f2s3s3Bteq8AOQjd14",
         "redirect_url" => "http://127.0.0.1:8080/callback",
         "code" => $request->code
 
